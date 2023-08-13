ce363a52dc244edfb24a21e49b9de54f4cfd9692
Kingsley Osuagwu-Chidiadi <chidiadi.kingsley@gmail.com>
2023-08-13 02:10:20 +0100

    Made code modular

--- assignment-2-servers-and-apis/apiServer.js
+++ assignment-2-servers-and-apis/apiServer.js
@@ -6,24 +6,20 @@ const port = 3002
 const filePath = `${__dirname}/data/items.json`
 const allItems = fs.readFileSync(filePath, 'utf-8')
 
+function responseHandler(res, code = 200, contentType='text/plain', data ) {
+    return res.writeHead(code, {
+        "Content-type": contentType
+    }).end(contentType == "application/json" ? JSON.stringify(data) : data)
+}
+
 const server = http.createServer((req, res) => {
     const {url, method} = req
     const parsedURL = nodeURL.parse(url, true)
 
     // Get all items list
     if ((url === '/v1/items') && (method === 'GET')) { 
-        try {
-            const file = JSON.parse(allItems)
-            res.writeHead(200,{
-                'Content-type': 'application/json'
-            }).end(JSON.stringify(file))
-            
-        } catch (error) {
-            console.warn(error)
-            return res.writeHead(404, {
-                'Content-type': 'text/html'
-            }).end(`<h1>WebPage Not Found</h1>`)
-        }
+        const file = JSON.parse(allItems)
+        return responseHandler(res, 200, 'application/json', file) // Return all items
     }
 
     // Get the details of a specific item by ID
@@ -36,27 +32,19 @@ const server = http.createServer((req, res) => {
                 const foundItem = items.find(item => id === item.id)
     
                 if (foundItem) {
-                    return res.writeHead(200, {
-                        'Content-type': 'application/json'
-                    }).end(JSON.stringify(foundItem))
+                    return responseHandler(res, 200, 'application/json', foundItem) // Return specific item
                 }
                 else {
-                    return res.writeHead(404, {
-                        'Content-type': 'text/html'
-                    }).end(`<p>Item not found</p>`)
+                    return responseHandler(res, 404, 'text/html', `<p>Item Not Found</p>`) // Item not found
                 }
             }
             else {
-                return res.writeHead(404, { 
-                    'Content-type': 'text/html'
-                }).end(`<h1>Page Not Found</h1>`)
+                return responseHandler(res, 404, 'text/html', `<h1>Page Not Found</h1>`) // Error Handling for bad search params and URLs
             }
         } 
         catch (error) {
             console.warn(error)
-            return res.writeHead(500, {
-                'Content-type': 'text/html'
-            }).end(`<p>Something went wrong</p>`)
+            return responseHandler(res, 500, 'text/html', `<p>Something went wrong</p>`) // Handle server errors
         }
     }
 
@@ -70,25 +58,19 @@ const server = http.createServer((req, res) => {
 
             if (itemIndex !== -1) { // If the item is found
                 const deletedItem = items.splice(itemIndex, 1) // Delete the item from the list
-
                 fs.writeFileSync(filePath, JSON.stringify(items)) // Write back to file
 
-                return res.writeHead(200, {      // Return OK response
-                    'Content-Type': 'application/json'
-                }).end(JSON.stringify(deletedItem))
+                return responseHandler(res, 200, 'application/json', deletedItem) // Return OK response
             }
             else {     // If the item is not found
-                return res.writeHead(404, { 
-                    'Content-Type': 'text/html' 
-                }).end('<p>Item not found</p>')
+                return responseHandler(res, 404, 'text/html', '<p>Item Not Found</p>') // Item not found
             } 
-        } catch (error) { // Handle any errors that are thrown from the try block
+        } 
+        catch (error) { 
+            // Handle any errors that are thrown from the try block
             console.warn(error)
-            return res.writeHead(404, {
-                'Content-type': 'text/html'
-            }).end(`<p>Something went wrong</p>`)
+            return responseHandler(res, 500, 'text/html', `<p>Something went wrong. Delete Error</p>`)
         }
-
     }
 
     // Create new entry
@@ -106,26 +88,19 @@ const server = http.createServer((req, res) => {
                     const parsedMessageBody = Buffer.concat(messageBody).toString() //Convert the buffer to string
                     
                     items.unshift(JSON.parse(parsedMessageBody)) // Send the newly created entry to the front
-    
                     fs.writeFileSync(filePath, JSON.stringify(items)) // Write the file to the disk
-    
-                    return res.writeHead(201, { 
-                        'Content-Type': 'application/json' 
-                    }).end(JSON.stringify(JSON.parse(parsedMessageBody)));
+
+                    return responseHandler(res, 201, 'application/json', JSON.parse(parsedMessageBody)) // Return response
                 }
-                else { // If there is no body with the POST request
-                    return res.writeHead(400, {
-                        'Content-Type': 'application/json'
-                    }).end(JSON.stringify({
-                        message: 'Please add a body to the request'
-                    }))
+                else {
+                    // If there is no body with the POST request, return Bad Request response
+                    return responseHandler(res, 400, 'application/json', {message: 'Please add a body to the request'}) 
                 } 
                 
             } catch (error) {
                 console.warn(error)
-                return res.writeHead(500, {
-                    "Content-type": "text/html"
-                }).end('<p>500 - Internal Server Error</p>') 
+                // Return Server Error
+                return responseHandler(res, 500, 'text/html', '<p>500 - Internal Server Error</p>') 
             }
         });
     }
@@ -146,31 +121,23 @@ const server = http.createServer((req, res) => {
                     const itemIndex = items.findIndex(item => item.id == id) // Find the index of the item
                     items[itemIndex] = {...items[itemIndex], ...JSON.parse(propertyToUpdate)} // Update the entry
                     fs.writeFileSync(filePath, JSON.stringify(items)) // Write the file back to the disk
-    
-                    return res.writeHead(202, { // Send an OK response
-                        'Content-Type': 'application/json' 
-                    }).end(JSON.stringify(JSON.parse(propertyToUpdate)));
+
+                    return responseHandler(res, 202, 'application/json', JSON.parse(propertyToUpdate)) // Send an OK response
                 }
-                else { // If there is no body attached to the PATCH request
-                    return res.writeHead(400, {
-                        'Content-Type': 'application/json'
-                    }).end(JSON.stringify({
-                        message: 'Please add a body to the request'
-                    }))
+                else { 
+                    // If there is no body attached to the PATCH request
+                    return responseHandler(res, 400, 'application/json', {message: 'Please add a body to the request'})
                 }
             } 
-            catch (error) { // Any other error will be caught here
+            catch (error) {
                 console.warn(error)
-                return res.writeHead(500, {
-                    "Content-type": "text/html"
-                }).end('<p>500 - Internal Server Error</p>')   
+                return responseHandler(res, 500, 'text/html', '<p>500 - Internal Server Error</p>') // Handle Server Errors
             }
         })
     }
-    else { // If all none of the allowed methods is sent to the API server
-        return res.writeHead(404, { 
-            'Content-type': 'text/html'
-        }).end(`<h1>Page Not Found</h1>`)
+    else { 
+        // If all none of the allowed methods is sent to the API server
+        return responseHandler(res, 404, 'text/html', `<h1>Page Not Found</h1>`)
     }
 })
 
